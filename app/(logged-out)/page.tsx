5cf86cf936af9edb591e5871b8e2d265c2a71f06
Maksim Sc <maksim.scerbuk@gmail.com>
2024-03-19 11:39:42 +0200

    Refactor font imports and update layout styles

---
+++ app/(logged-out)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+	return (
+		<div>
+			<h1>Page</h1>
+			<Button>Log In</Button>
+			<Button>Sign Up</Button>
+		</div>
+	);
+}
