6e8d266a4efd7f89ddb45117c44134c1c25f7919
Maksim Sc <maksim.scerbuk@gmail.com>
2024-03-19 12:52:22 +0200

    6. define global styles add buttons and icon

--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -1,11 +1,24 @@
 import { Button } from '@/components/ui/button';
+import { PersonStanding, PersonStandingIcon } from 'lucide-react';
+import Link from 'next/link';
 
 export default function Home() {
 	return (
-		<div>
-			<h1>Page</h1>
-			<Button>Log In</Button>
-			<Button>Sign Up</Button>
-		</div>
+		<>
+			<h1 className='flex items-center  gap-2 '>
+				<PersonStandingIcon size={50} className='text-pink-500' />
+				Page
+			</h1>
+			<p>Best dashbord for managing</p>
+			<div className='flex gap-2 items-center'>
+				<Button asChild>
+					<Link href='/login'>Log In</Link>
+				</Button>
+				<small>or</small>
+				<Button asChild variant='outline'>
+					<Link href='/sign-up'>Sign up</Link>
+				</Button>
+			</div>
+		</>
 	);
 }
