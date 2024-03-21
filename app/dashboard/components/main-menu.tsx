9183b3e4a7c030050e5f39ba5b326ae5ff8d9133
Wolfern <maksim.scerbuk@gmail.com>
2024-03-21 17:00:54 +0200

    43. Implement mobile menu, added to Drawer context to pass orientation of menu to DrawerContent

--- app/dashboard/components/main-menu.tsx
+++ app/dashboard/components/main-menu.tsx
@@ -11,8 +11,10 @@ interface MainMenuProps {
 
 export default function MainMenu({ className }: MainMenuProps) {
 	return (
-		<nav className={cn('bg-muted overflow-auto p-4 flex flex-col', className)}>
-			<header className='border-b dark:border-b-black border-b-zinc-300 pb-4'>
+		<nav
+			className={cn('md:bg-muted overflow-auto p-4 flex flex-col', className)}
+		>
+			<header className=' hidden md:block border-b dark:border-b-black border-b-zinc-300 pb-4'>
 				<MenuTitle />
 			</header>
 			<ul className='pt-4 grow'>
