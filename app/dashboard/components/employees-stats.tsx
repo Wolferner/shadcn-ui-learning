8259534f80fbaf7d4370bde1dab0b1f239d58f23
Wolfern <maksim.scerbuk@gmail.com>
2024-03-21 12:55:32 +0200

    31. Updated cards in employees-stats

--- app/dashboard/components/employees-stats.tsx
+++ app/dashboard/components/employees-stats.tsx
@@ -1,11 +1,37 @@
-import { Card } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { UserIcon } from 'lucide-react';
+import Link from 'next/link';
 
 export default function EmployeesStats() {
 	return (
 		<div className='grid lg:grid-cols-3 gap-4'>
-			<Card>card1</Card>
-			<Card>card2</Card>
-			<Card>card3</Card>
+			<Card>
+				<CardHeader>
+					<CardTitle className='text-base'>Total Employees</CardTitle>
+				</CardHeader>
+				<CardContent className='flex justify-between items-center '>
+					<div className='flex gap-2'>
+						<UserIcon />
+						<div className='text-5xl font-bold'>100</div>
+					</div>
+					<div>
+						<Button size='sm' asChild>
+							<Link href='/dashboard/employees'>View All</Link>
+						</Button>
+					</div>
+				</CardContent>
+			</Card>
+			<Card>
+				<CardHeader>
+					<CardTitle className='text-base'>Employees present</CardTitle>
+				</CardHeader>
+			</Card>
+			<Card className='border-pink-500'>
+				<CardHeader>
+					<CardTitle className='text-base'>Employee of the month</CardTitle>
+				</CardHeader>
+			</Card>
 		</div>
 	);
 }
