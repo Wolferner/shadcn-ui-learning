964d3f3e3c7cb1306412f7b32dec42feb9e20363
Wolfern <maksim.scerbuk@gmail.com>
2024-03-21 12:28:34 +0200

    29. Added tabs

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,14 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
 export default function Dashboard() {
-	return <div>Dashboard</div>;
+	return (
+		<Tabs>
+			<TabsList>
+				<TabsTrigger value='employees'>Employees stats</TabsTrigger>
+				<TabsTrigger value='teams'>Teams stats</TabsTrigger>
+			</TabsList>
+			<TabsContent value='employees'>1</TabsContent>
+			<TabsContent value='teams'>2</TabsContent>
+		</Tabs>
+	);
 }
